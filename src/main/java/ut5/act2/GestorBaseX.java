f8eb7502995f882b06c0c4dede368861c4de1a62
Pau Aldea <paualdeabatista@protonmail.com>
2026-04-26 10:46:51 +0200

    Creación repositorio

---
+++ src/main/java/ut5/act2/GestorBaseX.java
@@ -0,0 +1,13 @@
+package ut5.act2;
+
+/**
+ * Clase que gestiona todas las operaciones que se realizan sobre el servidor BaseX.
+ */
+public class GestorBaseX {
+    /**
+     * Constructor de la clase. Inicializa la conexión entre la aplicación y la DB.
+     */
+    public GestorBaseX (String servidor, int puerto, String usuario, String passwd) {
+
+    }
+}
---
+++ src/main/java/ut5/act2/Main.java
@@ -0,0 +1,10 @@
+package ut5.act2;
+
+public class Main {
+    public static void main(String[] args) {
+        // Creamos un gestor de BaseX con sus parámetros
+        GestorBaseX gestor = new GestorBaseX("localhost", 1984, "admin", "admin");
+
+
+    }
+}

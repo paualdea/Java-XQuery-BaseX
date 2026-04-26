976a4dca5890c03421a2ac5fb570ff57bd9a22b1
Pau Aldea <paualdeabatista@protonmail.com>
2026-04-26 11:56:27 +0200

    Completadas funciones conexión y creación DB Closes #1, #2

--- src/main/java/ut5/act2/GestorBaseX.java
+++ src/main/java/ut5/act2/GestorBaseX.java
@@ -1,13 +1,45 @@
 package ut5.act2;
 
+import org.basex.api.client.ClientSession;
+
+import java.io.IOException;
+
 /**
  * Clase que gestiona todas las operaciones que se realizan sobre el servidor BaseX.
  */
 public class GestorBaseX {
+    // Creamos la sesión de conexión al servidor de BaseX
+    private ClientSession sesion;
+
     /**
      * Constructor de la clase. Inicializa la conexión entre la aplicación y la DB.
-     */
-    public GestorBaseX (String servidor, int puerto, String usuario, String passwd) {
 
+     * @throws IOException
+     * Controlamos la excepción de entrada y salida que puede provocar la sesión de conexión a la DB.
+     */
+    public GestorBaseX (String servidor, int puerto, String usuario, String passwd) throws IOException {
+        // Creamos la sesión usando los parámetros recibidos
+        sesion = new ClientSession(servidor, puerto, usuario, passwd);
+        System.out.println("Conexión al servidor BaseX completada.");
     }
+
+    /**
+     * Función para crear una BD a partir de un XML.
+
+     * @param xml
+     * Recibe un XML estructurado para crear la BD.
+     * @throws IOException
+     * Controlamos la excepción de entrada y salida que puede provocar la sesión de conexión a la DB.
+     */
+    public void crearBD(String xml) throws IOException {
+        try {
+            // Ejecutamos una sentencia de creación de DB (AutoresDB) pasando el XML recibido cómo parámetro.
+            sesion.execute("CREATE DB AutoresDB " + xml);
+            System.out.println("DB AutoresDB creada.");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
 }
--- src/main/java/ut5/act2/Main.java
+++ src/main/java/ut5/act2/Main.java
@@ -1,10 +1,23 @@
 package ut5.act2;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Creamos un gestor de BaseX con sus parámetros
         GestorBaseX gestor = new GestorBaseX("localhost", 1984, "admin", "admin");
 
+        // XML Estructurado para crear en el servidor
+        String xml = "<autores>" +
+                "<autor id='1'>" +
+                "<nombre>Ana Pérez</nombre><nacionalidad>España</nacionalidad>" +
+                "</autor>" +
+                "<autor id='2'>" +
+                "<nombre>John Smith</nombre><nacionalidad>Reino Unido</nacionalidad>" +
+                "</autor>" +
+                "</autores>";
 
+        // Creamos la BD pasando cómo parámetro el XML
+        gestor.crearBD(xml);
     }
 }
